d22b4cba60547a78c7fb685a1d14b92c2b71904e
Jagrat <jagratgandhi0@gmail.com>
2025-04-12 15:01:57 +0530

    change in branch

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello, Express!');
 });
 
+app.get('/home', (req, res) => {
+  res.send('Home, Express!');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
